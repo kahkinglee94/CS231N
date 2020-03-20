50c595bc9d9a840a6d02db90c2e9a236a7a94c7c
Kingz <lekingz94@gmail.com>
2020-03-20 17:54:39 +0800

    Added own softmax_loss function to Unit_Test

---
+++ Unit_Test/softmax_loss.py
@@ -0,0 +1,278 @@
+import numpy as np
+
+
+def __init__(self, input_dim=3 * 32 * 32, hidden_dim=100, num_classes=10,
+             weight_scale=1e-3, reg=0.0):
+
+    self.params['W1'] = weight_scale * np.random.randn(input_dim, hidden_dim)
+    self.params['W2'] = weight_scale * np.random.randn(hidden_dim, num_classes)
+    self.params['b1'] = np.zeros(hidden_dim)
+    self.params['b2'] = np.zeros(num_classes)
+
+
+def loss(self, X, y=None):
+
+    scores = None
+    W1 = self.params['W1']
+    W2 = self.params['W2']
+    b1 = self.params['b1']
+    b2 = self.params['b2']
+    ############################################################################
+    # TODO: Implement the forward pass for the two-layer net, computing the    #
+    # class scores for X and storing them in the scores variable.              #
+    ############################################################################
+    X2, relu_cache = affine_relu_forward(X, W1, b1)
+    scores, relu2_cache = affine_relu_forward(X2, W2, b2)
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    # If y is None then we are in test mode so just return scores
+    if y is None:
+        return scores
+
+    loss, grads = 0, {}
+    ############################################################################
+    # TODO: Implement the backward pass for the two-layer net. Store the loss  #
+    # in the loss variable and gradients in the grads dictionary. Compute data #
+    # loss using softmax, and make sure that grads[k] holds the gradients for  #
+    # self.params[k]. Don't forget to add L2 regularization!                   #
+    #                                                                          #
+    # NOTE: To ensure that your implementation matches ours and you pass the   #
+    # automated tests, make sure that your L2 regularization includes a factor #
+    # of 0.5 to simplify the expression for the gradient.                      #
+    ############################################################################
+    # calculate loss
+    loss, softmax_grad = softmax_loss(scores, y)
+    loss += 0.5 * self.reg * (np.sum(W1 * W1) + np.sum(W2 * W2))
+
+    # calculate gradient
+    dx2, dw2, db2 = affine_relu_backward(softmax_grad, relu2_cache)
+    dx, dw, db = affine_relu_backward(dx2, relu_cache)
+    grads['W2'] = dw2 + self.reg * W2
+    grads['b2'] = db2
+    grads['W1'] = dw + self.reg * W1
+    grads['b1'] = db
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    return loss, grads
+
+
+def softmax_loss1(x, y):
+    """
+    Computes the loss and gradient for softmax classification.
+    Inputs:
+    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth
+      class for the ith input.
+    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+      0 <= y[i] < C
+    Returns a tuple of:
+    - loss: Scalar giving the loss
+    - dx: Gradient of the loss with respect to x
+    """
+    # tmp = np.max(x, axis=1, keepdims=True)
+    shifted_logits = x - np.max(x, axis=1, keepdims=True)
+    Z = np.sum(np.exp(shifted_logits), axis=1, keepdims=True)
+    log_probs = shifted_logits - np.log(Z)
+    probs = np.exp(log_probs)
+    N = x.shape[0]
+    # tmp2 = np.arange(N)
+    tmp3 = log_probs[np.arange(N), y]
+    # tmp4 = log_probs[[0,1,2],[2,5,0]]
+    loss = -np.sum(log_probs[np.arange(N), y]) / N
+    dx = probs.copy()
+    dx[np.arange(N), y] -= 1
+    dx /= N
+    return loss, dx
+
+
+def affine_forward(x, w, b):
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
+    Inputs:
+    - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    - w: A numpy array of weights, of shape (D, M)
+    - b: A numpy array of biases, of shape (M,)
+    Returns a tuple of:
+    - out: output, of shape (N, M)
+    - cache: (x, w, b)
+    """
+    out = None
+    ###########################################################################
+    # TODO: Implement the affine forward pass. Store the result in out. You   #
+    # will need to reshape the input into rows.                               #
+    ###########################################################################
+    dim_size = x[0].shape
+    X = x.reshape(x.shape[0], np.prod(dim_size))
+    out = X.dot(w) + b
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    cache = (x, w, b)
+    return out, cache
+
+
+def affine_backward(dout, cache):
+    """
+    Computes the backward pass for an affine layer.
+    Inputs:
+    - dout: Upstream derivative, of shape (N, M)
+    - cache: Tuple of:
+      - x: Input data, of shape (N, d_1, ... d_k)
+      - w: Weights, of shape (D, M)
+      - b: Biases, of shape (M,)
+    Returns a tuple of:
+    - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    - dw: Gradient with respect to w, of shape (D, M)
+    - db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, b = cache
+    dx, dw, db = None, None, None
+    ###########################################################################
+    # TODO: Implement the affine backward pass.                               #
+    ###########################################################################
+    dim_shape = np.prod(x[0].shape)
+    N = x.shape[0]
+    X = x.reshape(N, dim_shape)
+    # input gradient
+    dx = dout.dot(w.T)
+    dx = dx.reshape(x.shape)
+    # weight gradient
+    dw = X.T.dot(dout)
+    # bias gradient
+    db = dout.sum(axis=0)
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return dx, dw, db
+
+
+def relu_forward(x):
+    """
+    Computes the forward pass for a layer of rectified linear units (ReLUs).
+    Input:
+    - x: Inputs, of any shape
+    Returns a tuple of:
+    - out: Output, of the same shape as x
+    - cache: x
+    """
+    out = None
+    ###########################################################################
+    # TODO: Implement the ReLU forward pass.                                  #
+    ###########################################################################
+    out = np.maximum(0, x)
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    cache = x
+    return out, cache
+
+
+def relu_backward(dout, cache):
+    """
+    Computes the backward pass for a layer of rectified linear units (ReLUs).
+    Input:
+    - dout: Upstream derivatives, of any shape
+    - cache: Input x, of same shape as dout
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    dx, x = None, cache
+    ###########################################################################
+    # TODO: Implement the ReLU backward pass.                                 #
+    ###########################################################################
+    dx = dout * (x > 0)
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return dx
+
+
+def affine_relu_forward(x, w, b):
+    """
+    Convenience layer that performs an affine transform followed by a ReLU
+    Inputs:
+    - x: Input to the affine layer
+    - w, b: Weights for the affine layer
+    Returns a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to the backward pass
+    """
+    a, fc_cache = affine_forward(x, w, b)
+    out, relu_cache = relu_forward(a)
+    cache = (fc_cache, relu_cache)
+    return out, cache
+
+
+def affine_relu_backward(dout, cache):
+    """
+    Backward pass for the affine-relu convenience layer
+    """
+    fc_cache, relu_cache = cache
+    da = relu_backward(dout, relu_cache)
+    dx, dw, db = affine_backward(da, fc_cache)
+    return dx, dw, db
+
+def softmax_loss2(x, y):
+    """
+    :param x: input from previous layer. Has shape [N, C]
+        - x[i, j] is the score for the jth class of the ith input
+        - N: minibatch size
+        - C: number of classes
+    :param y: labels. Has shape [N, ]
+    :return:
+        - loss
+        - gradient with respect to loss
+    """
+    # Compute loss
+    batch_num = x.shape[0]
+    shifted_logits = x - np.max(x, axis=1, keepdims=True)
+    h = np.sum(np.exp(shifted_logits), axis=1, keepdims=True)
+    k = shifted_logits[np.arange(batch_num), y]
+    k = k.reshape(batch_num, 1)
+    probs = -k + np.log(h)
+    loss = np.sum(probs) / batch_num
+
+    #Compute gradient
+    exp_scores = np.exp(shifted_logits)
+    normalized_score = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    normalized_score[np.arange(batch_num), y] -= 1
+    normalized_score /= batch_num
+    dx = normalized_score
+    return loss, dx
+
+
+num_inputs = 2
+input_shape = (4, 5, 6)
+output_dim = 10
+
+input_size = num_inputs * np.prod(input_shape)
+weight_size = output_dim * np.prod(input_shape)
+
+x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs, *input_shape)
+w = np.linspace(-0.2, 0.3, num=weight_size).reshape(np.prod(input_shape), output_dim)
+b = np.linspace(-0.3, 0.1, num=output_dim)
+
+out1, affine_cache1 = affine_forward(x, w, b)
+
+out2, affine_cache2 = relu_forward(out1)
+
+np.random.seed(231)
+num_classes, num_inputs = 10, 50
+x = 0.001 * np.random.randn(num_inputs, num_classes)
+y = np.random.randint(num_classes, size=num_inputs)
+loss1, dx1 = softmax_loss1(x, y)
+loss2, dx2 = softmax_loss2(x, y)
+print("Example softmax_loss")
+print(loss1)
+print(dx1)
+print("\n")
+print("Own softmax_loss")
+print(loss2)
+print(dx2)
