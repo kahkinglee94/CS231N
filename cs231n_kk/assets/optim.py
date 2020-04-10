7cb7054ab8c4a90b4bbc35e4100f3060cb4e70fa
Kingz <lekingz94@gmail.com>
2020-04-10 11:57:11 +0800

    Added stochastic gradient descent for optimization function

---
+++ cs231n_kk/assets/optim.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def sgd(w, dw, config=None):
+    """
+    Performs vanilla stochastic gradient descent.
+    config format:
+    - learning_rate: Scalar learning rate.
+    """
+    if config is None:
+        config = {}
+
+    # setDefault method returns the value of a key (if the key is in dictionary). If not, it inserts key with a value to the dictionary.
+    config.setdefault('learning_rate', 1e-2)
+    w -= config['learning_rate'] * dw
+    return w, config
+
