e383c3d3224e58c8e00d3c1107e5f28c99cff0c4
Kingz <lekingz94@gmail.com>
2020-05-04 17:46:22 +0800

    Added droupout layer

--- cs231n_kk/assets/layers.py
+++ cs231n_kk/assets/layers.py
@@ -198,6 +198,54 @@ def layernorm_forward(x, gamma, beta, ln_param):
     return out, cache
 
 
+def dropout_forward(x, dropout_param):
+    """
+        Performs the forward pass for (inverted) dropout.
+        Inputs:
+        - x: Input data, of any shape
+        - dropout_param: A dictionary with the following keys:
+          - p: Dropout parameter. We keep each neuron output with probability p.
+          - mode: 'test' or 'train'. If the mode is train, then perform dropout;
+            if the mode is test, then just return the input.
+          - seed: Seed for the random number generator. Passing seed makes this
+            function deterministic, which is needed for gradient checking but not
+            in real networks.
+        Outputs:
+        - out: Array of the same shape as x.
+        - cache: tuple (dropout_param, mask). In training mode, mask is the dropout
+          mask that was used to multiply the input; in test mode, mask is None.
+        NOTE: Please implement **inverted** dropout, not the vanilla version of dropout.
+        See http://cs231n.github.io/neural-networks-2/#reg for more details.
+        NOTE 2: Keep in mind that p is the probability of **keep** a neuron
+        output; this might be contrary to some sources, where it is referred to
+        as the probability of dropping a neuron output.
+        """
+    p, mode = dropout_param['p'], dropout_param['mode']
+    if 'seed' in dropout_param:
+        np.random.seed(dropout_param['seed'])
+
+    mask = None
+    out = None
+
+    if mode == 'train':
+        #######################################################################
+        # TODO: Implement training phase forward pass for inverted dropout.   #
+        # Store the dropout mask in the mask variable.                        #
+        #######################################################################
+        mask = (np.random.randn(*x.shape) < p) / p
+        out = x * mask
+
+    elif mode == 'test':
+        #######################################################################
+        # TODO: Implement the test phase forward pass for inverted dropout.   #
+        #######################################################################
+        out = x
+
+    cache = (dropout_param, mask)
+    out = out.astype(x.dtype, copy=False)
+
+    return out, cache
+
 
 def softmax_loss(x, y):
     """
